74a12d788f9a6c7005275ff3788aea9521d2d6fe
Elv310 <Elvisa310@gmail.com>
2021-11-30 20:36:05 -0500

    Starter Code for Spring boot API

---
+++ src/main/java/com/careerdevs/jsonplaceholder/JsonplaceholderApplication.java
@@ -0,0 +1,16 @@
+package com.careerdevs.jsonplaceholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class JsonplaceholderApplication {
+	public RestTemplate
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonplaceholderApplication.class, args);
+	}
+
+}
+
