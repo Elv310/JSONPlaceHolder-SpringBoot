294f35098463cf5bcf8fe3479c7edbfab2f723e9
Elv310 <Elvisa310@gmail.com>
2021-11-30 20:45:58 -0500

    RestTemplate added

--- src/main/java/com/careerdevs/jsonplaceholder/JsonplaceholderApplication.java
+++ src/main/java/com/careerdevs/jsonplaceholder/JsonplaceholderApplication.java
@@ -2,11 +2,14 @@ package com.careerdevs.jsonplaceholder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JsonplaceholderApplication {
-	public RestTemplate
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsonplaceholderApplication.class, args);
